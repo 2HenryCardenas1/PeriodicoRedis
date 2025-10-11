e577e925dc35afa13f119279ef74539acb5367cb
H3C4 <cardenashenryeseteban@gmail.com>
2025-10-11 14:32:16 -0500

    Repositorio creado

---
+++ src/main/java/com/uptc/frw/periodicoredis/PeriodicoRedisApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.periodicoredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicoRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeriodicoRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/periodicoredis/PeriodicoRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.periodicoredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicoRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
