5e0b0d812d65eff1c81a00e28b7416cb821e5629
H3C4 <cardenashenryeseteban@gmail.com>
2025-10-11 14:49:09 -0500

    Dependencias instaladas, corriendo por el puerto 8081

--- src/main/java/com/uptc/frw/periodicoredis/PeriodicoRedisApplication.java
+++ src/main/java/com/uptc/frw/periodicoredis/PeriodicoRedisApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PeriodicoRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PeriodicoRedisApplication.class, args);
+
+
+        SpringApplication.run(PeriodicoRedisApplication.class, args
+
+
+        );
     }
 
 }
